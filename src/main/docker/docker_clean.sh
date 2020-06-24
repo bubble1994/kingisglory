5b2cdfb065af2f1aa201cc170971f1e61404b4db
liuzhichao <892556445@qq.com>
2020-06-25 04:28:34 +0800

    feat：新功能（feature）医院项目
    fix：修补bug 新增数据库
    
    docs：文档（documentation）
    
    style： 格式（不影响代码运行的变动）
    
    refactor：重构（即不是新增功能，也不是修改bug的代码变动）
    
    test：增加测试
    
    chore：构建过程或辅助工具的变动

--- src/main/docker/docker_clean.sh
+++ src/main/docker/docker_clean.sh
@@ -3,3 +3,6 @@ docker rm -f `docker ps -a|grep kingisglory|awk '{print $1}'`;
 
 # 删除镜像
 docker rmi -f `docker images|grep kingisglory|awk '{print $3}'`
+
+
+#java -jar /var/jenkins_home/workspace/kingisglory/target/kingisglory-0.0.1-SNAPSHOT.jar
